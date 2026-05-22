45f41df8bfecd1d92518ff75e636a5792fc2a1f8
Patty2026 <pattyherrerahdz@hotmail.com>
2026-05-22 14:08:15 -0600

    Agregar configuración base Firebase

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Configuración Firebase para Conecta Martínez
+// Paso 1: Crear proyecto en https://console.firebase.google.com/
+// Paso 2: Registrar una app Web
+// Paso 3: Copiar aquí las credenciales reales que entrega Firebase
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+export default firebaseConfig;
